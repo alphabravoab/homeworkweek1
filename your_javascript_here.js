e4d6bfbc56aadf0d1d980b378a6d6b87dbae5c18
Arjen <alphabravoab@gmail.com>
2018-06-01 21:21:35 +0200

    first commit

---
+++ your_javascript_here.js
@@ -0,0 +1,14 @@
+// Variables
+let hero= {}
+
+hero.name="aa"
+hero.heroic=true
+hero.health= 1000
+hero.inventory=[]
+hero.weapon={}
+hero.weapon.type="Sink"
+hero.weapon.damage= 120
+
+    //Game logic
+
+// UI
